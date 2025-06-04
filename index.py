b99468c8ce85643c591514231c68b46f5e1e8944
Nahuel Ayala <Nahuel.Ayala@bplay.com.ar>
2025-06-04 20:58:16 -0300

    Se agrega comentarios y funcion de evaluacion de condiciones logicas.

--- index.py
+++ index.py
@@ -22,7 +22,9 @@ mensaje_operacion = f'''Ingrese la operación a realizar con los conjuntos:
                   2: Intersección
                   3: Diferencias
                   4: Diferencia simétrica
-                  5: Suma total de los dígitos de los DNI
+                  5: Frecuencia de dígitos en los DNI
+                  6: Suma total de los dígitos de los DNI
+                  7: Evaluación de condiciones lógicas
                   0: Finalizar\n'''
 
 operación = input(f'{mensaje_operacion}')
@@ -32,39 +34,56 @@ while operación != 0:
     if operación == "1":
         conjunto1 = input((f'\nIngrese el primer conjunto:\n{utils.clave_valor_string(conjuntos_unicos)}\b'))
         conjunto2 = input((f'\nIngrese el segundo conjunto:\n{utils.clave_valor_string(conjuntos_unicos)}\b'))
-        
+        #al operar con conjuntos es posible utilizar el metodo "union" de set para calcular o el operador + 
         union = conjuntos_unicos[int(conjunto1)].union(conjuntos_unicos[int(conjunto2)])
         print(f"\nUnión: {union}\n")
     
     elif operación == "2":
         conjunto1 = input((f'\nIngrese el primer conjunto:\n{utils.clave_valor_string(conjuntos_unicos)}\b'))
         conjunto2 = input((f'\nIngrese el segundo conjunto:\n{utils.clave_valor_string(conjuntos_unicos)}\b'))
-        
+        #set tiene el metodo intersección para operar entre conjuntos o con el operador &
         interseccion = conjuntos_unicos[int(conjunto1)].intersection(conjuntos_unicos[int(conjunto2)])
         print(f"\nInterseccion: {interseccion}\n")
 
     elif operación == "3":
         conjunto1 = input((f'\nIngrese el primer conjunto:\n{utils.clave_valor_string(conjuntos_unicos)}\b'))
         conjunto2 = input((f'\nIngrese el segundo conjunto:\n{utils.clave_valor_string(conjuntos_unicos)}\b'))
-        
+        #se utiliza el motodo de set difference para obtener la diferencia entre conjuntos, o usando el operador -
         diferencia = conjuntos_unicos[int(conjunto1)].difference(conjuntos_unicos[int(conjunto2)])
         print(f"\nDiferencia: {diferencia}\n")
 
     elif operación == "4":
         conjunto1 = input((f'\nIngrese el primer conjunto:\n{utils.clave_valor_string(conjuntos_unicos)}\b'))
         conjunto2 = input((f'\nIngrese el segundo conjunto:\n{utils.clave_valor_string(conjuntos_unicos)}\b'))
-        
+        #set tambien cuenta con el metodo symmetric_difference tambien posible con el operador ^
         diferencia_simetrica = conjuntos_unicos[int(conjunto1)].symmetric_difference(conjuntos_unicos[int(conjunto2)])
         print(f"\nDiferencia simetrica: {diferencia_simetrica}\n")
 
     elif operación == "5":
+        #se recorre los dnis ingresados para crear un string con todos los digitos
+        digitos = ""
+        for dni in conjuntos_str:
+            digitos += dni
+        #al string con los digitos se pasa como argumento a la funcion auxiliar para que entre la frecuencia
+        print(f'La frecuencia de digitos en los DNI son:\n{utils.frecuencia_digitos(digitos)}')
+    
+    elif operación == "6":
+        #se pasa como argumento los dnis ingresados a la funcion auxiliar para luego imprimir
+        #cada dni con su suma de digitos correspondiente.
         suma_dnis = utils.suma_digitos(conjuntos_str)
         print(f'Sumatoria de los digitos de los DNI: \n{utils.clave_valor_string(suma_dnis)}')
-
+    
+    elif operación == "7":
+        #se utilizan dos funciones auxiliares para determinar el digito compartido entre los conjuntos creados
+        # y la diversidad numerica de cada uno. 
+        digito_compartido = utils.digito_compartido(conjuntos_unicos)
+        diversidad = utils.diversidad_numerica(conjuntos_unicos)
+    #Si el usuario ingresa la opción 0 el programa finaliza.
     elif operación == "0":
         print("\nGracias por usar la calculadora de conjuntos.")
         break
     else:
+    #si el usuario no ingresa una opcion correcta, da aviso y vuelve a preguntar
         print("Ingrese una opción correcta, por favor.")
-
+    #se pregunta nuevamente al usuario una opción para que vuelva a correr el bucle
     operación = input(f'{mensaje_operacion}')
--- utils.py
+++ utils.py
@@ -1,18 +1,58 @@
 #auxiliar para convertir un dic en un strin de clave, valor para imprimir en pantalla
 def clave_valor_string(diccionario):
     clave_valor = ""
+    #se recorre el diccionario tomando todos los items para obtener cada key, value
+    #para poder imprimirlo
     for clave, valor in diccionario.items():
         clave_valor += f"{clave}: {valor}\n"
 
     return clave_valor
 
+#funcion auxiliar para encontrar la frecuencia de digitos 
+def frecuencia_digitos(digitos): 
+    frecuencia_dic = {}
+    #recorre los digitos del argumento, se utiliza el metodo get() de diccionarios
+    # y se pasa como argumento un value por defecto para que no de error si no esta el indice buscado
+    #si lo encuentra le suma uno al valor del indice
+    for d in digitos:
+        frecuencia_dic[d] = frecuencia_dic.get(d, 0) + 1
+    
+    return frecuencia_dic
+
+#funcion auxiliar para sumar los digitos de una lista 
 def suma_digitos(list_dni):
     sum_dic = {}
+    #contador para usarlo como indice del diccionario
     contador = 1
+    #recorre la lista
     for dni in list_dni:
         suma = 0
+        #recorre cada digito para luego sumarlo en la variable suma
         for n in dni:
             suma += int(n)
+        #se utilizar el indice del contador para agregar al diccionario la suma de los digitos
         sum_dic[contador] = suma
         contador += 1
+    #retorna un diccionario con indice y suma de cada dni en la lista
     return sum_dic
+
+#auxiliar para encontrar los digitos compartidos en el diccionario de conjuntos
+def digito_compartido(conjuntos): 
+    #se utiliar el metodo intersection de set para encontrar los digitos compartidos
+    #se pasa como argumento todos los valores dentro del diccionario conjuntos con el operador * 
+    interseccion = set.intersection(*conjuntos.values())
+    
+    if interseccion:
+        print(f"Digitos compartidos: {interseccion}")
+    else:
+        print("No comparten ningun digito.")       
+
+#funcion auxiliar para determinar si el conjunto es de divesidad numerica alta o baja
+def diversidad_numerica(conjuntos):
+    #se recorrer todos los valores del dictionario pasado como argumento
+    for c in conjuntos.values():
+        #se determina la diversidad numerica dependiendo de la longitud del elementos del conjunto
+        if len(c) > 6:
+            print(f"El conjunto {c} tiene diversidad númerica alta.")
+        else: 
+            print(f"El conjunto {c} tiene una diversidad númerica baja.")
